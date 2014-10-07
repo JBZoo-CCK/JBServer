633676d137a093dd15ef18caf0436755d859a61e
smetdenis <admin@jbzoo.com>
2014-10-08 01:09:36 +0700

    config require

--- config.php
+++ config.php
@@ -2,11 +2,13 @@
 
 //jbdump::showErrors();
 
-if ($_SERVER['HTTP_HOST'] == 'jbhostchecker') {
-    define('DB_HOST', '127.0.0.1');
-    define('DB_NAME', 'test');
-    define('DB_USER', 'root');
-    define('DB_PORT', '3306');
-    define('DB_PASS', '');
-}
+die('Please, edit <u>config.php</u> before any tests!');
+
+/*
+define('DB_HOST', '127.0.0.1');
+define('DB_NAME', 'test');
+define('DB_USER', 'root');
+define('DB_PORT', '3306');
+define('DB_PASS', '');
+*/
 

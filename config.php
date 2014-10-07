981b6aae6fdaa1a5bee7494018527c40e6a5f666
smetdenis <admin@jbzoo.com>
2014-10-08 01:13:15 +0700

    add comment

--- config.php
+++ config.php
@@ -1,14 +1,14 @@
 <?php
 
-//jbdump::showErrors();
 
-die('Please, edit <u>config.php</u> before any tests!');
 
-/*
+die('Please, edit <u>config.php</u> before any tests!'); // delete me
+
+
 define('DB_HOST', '127.0.0.1');
 define('DB_NAME', 'test');
 define('DB_USER', 'root');
 define('DB_PORT', '3306');
 define('DB_PASS', '');
-*/
+
 
